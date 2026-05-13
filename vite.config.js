28205ff8b98b61302a2956bfd62fe7949b32ad92
iwtkai <sv.mu.g.em.d.9.xxx@gmail.com>
2026-05-14 02:36:27 +0900

    fix: baseパスをリポジトリ名に合わせて修正

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/conv-assist-app/", // ← GitHubリポジトリ名に合わせてください
+  base: "/", // ← GitHubリポジトリ名に合わせてください
 });
